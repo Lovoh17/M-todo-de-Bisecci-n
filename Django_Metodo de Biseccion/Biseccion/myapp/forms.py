8e8edb4e39cdad45a4e2760e32653e75fde7a57e
Lovoh17 <115592738+Lovoh17@users.noreply.github.com>
2024-06-12 20:50:30 -0600

    Delete Django_Metodo de Biseccion/Biseccion directory

--- Django_Metodo de Biseccion/Biseccion/myapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class BiseecionForm(forms.Form):
-    Ec_values = forms.CharField(label='Escriba la ecuacion:', widget=forms.TextInput(attrs={'placeholder': 'Ejemplo: x**2+5*3x'}))
-    valor_min = forms.CharField(label='Ingrese el rango mínimo de búsqueda:', widget=forms.TextInput(attrs={'placeholder': 'Ejemplo: -3'}))
-    valor_max = forms.CharField(label='Ingrese el rango máximo de búsqueda', widget=forms.TextInput(attrs={'placeholder': 'Ejemplo: 3'}))
-    step = forms.CharField(label='Ingrese el tamaño del paso para buscar intervalos: ', widget=forms.TextInput(attrs={'placeholder': 'Ejemplo: 1'}))
-    error_porcentual = forms.CharField(label='Ingrese el valor de le Tolerancia', widget=forms.TextInput(attrs={'placeholder': 'Ejemplo: 0.001'}))
-
