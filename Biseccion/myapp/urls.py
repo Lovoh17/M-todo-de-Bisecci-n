be839ea5dc27fb1f68d5c2dd8cd224da5a3c9ffa
Lovoh17 <115592738+Lovoh17@users.noreply.github.com>
2024-06-12 20:50:12 -0600

    Delete Biseccion directory

--- Biseccion/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-#from django.urls import include, path
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('/Biseccion/myapp/templates/Biseccion/calcular_biseccion.html', views.calcular_biseccion, name='calcular_Biseccion'),
-]
